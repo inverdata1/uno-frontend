6565af6cc429b17e735df0b469baee834729e503
JustDespair <best.off.alive@gmail.com>
2026-09-22 13:57:21 -0400

    fix(pm2): use expo start --host lan and pass REACT_NATIVE_PACKAGER_HOSTNAME via pm2 env

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,11 +3,12 @@ module.exports = {
     {
       name: 'expo-mobile-uno',
       script: 'npx',
-      args: 'expo start --public',
+      args: 'expo start --host lan',
       cwd: './',
       watch: false,
       env: {
         NODE_ENV: 'development',
+        REACT_NATIVE_PACKAGER_HOSTNAME: '2.24.223.151',
         EXPO_ROUTER_DISABLE_RN_NAVIGATION_CHECK: '1',
       },
     },
